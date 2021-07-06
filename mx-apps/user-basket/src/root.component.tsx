14a52dfdd76e1d645b593a787c6575506c632104
Aaron Zhao <aaron.zhao@asurion.com>
2021-07-06 06:16:10 -0800

    Fix tests on props name property

--- mx-apps/user-basket/src/root.component.tsx
+++ mx-apps/user-basket/src/root.component.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 
 export default function Root(props) {
+  const title = props.name?.split('/')[1]?.split('-').map(w => w[0].toUpperCase() + w.slice(1)).join(' ');
   return (
     <div>
-      <h1>{ props.name.split('/')[1].split('-').map(w => w[0].toUpperCase() + w.slice(1)).join(' ') }</h1>
+      <h1>{ title }</h1>
       <p>{ props.name } is mounted!</p>
     </div>
   );
